b017daedce95de98127d8fa68a6c869b7d46f2d2
HemanthReddyKodakandla-bootcamp <hemanthreddykodakandla@gmail.com>
2019-09-10 11:11:38 +0530

    project

---
+++ flutter_shoppingui/lib/view/shopping_home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ShoppingHomeView extends StatefulWidget {
+  @override
+  _ShoppingHomeViewState createState() => _ShoppingHomeViewState();
+}
+
+class _ShoppingHomeViewState extends State<ShoppingHomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
